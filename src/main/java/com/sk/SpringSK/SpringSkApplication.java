8434d9839b90b095f0b58281fadff91523ef546f
YoonKeumJae <ykj1512@gmail.com>
2025-03-11 16:34:56 +0900

    init

---
+++ src/main/java/com/sk/SpringSK/SpringSkApplication.java
@@ -0,0 +1,13 @@
+package com.sk.SpringSK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSkApplication.class, args);
+	}
+
+}
